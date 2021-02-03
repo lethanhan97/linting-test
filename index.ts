84763fee6cd1d1ac8bfc1321c07cae131cb3bba2
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-03 23:11:34 +0800

    added basic server

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-const test: string = 'hi';
+import express from 'express';
 
-console.log(test);
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello world'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port ${PORT}`);
+});
