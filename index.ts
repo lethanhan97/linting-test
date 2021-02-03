1ff3581d4fc9311d02031a1cd6878f8e7b3ff70d
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-03 23:39:02 +0800

    changed unused argument

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.status(200).json({
     message: 'Hello world'
   });
